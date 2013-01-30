b22ee236d7b12986fb403db2def18d39340e0ea4
Rafael Pellon <rpellon@tid.es>
2013-01-30 08:58:23 +0100

    WordCount

---
+++ wordcount.py
@@ -0,0 +1,12 @@
+# Cuantas veces ocurre cada palabra en un
+# fichero de texto
+def mapper(key,value):
+  for word in value.split():
+    yield word,1
+
+def reducer(key,values):
+  yield key,sum(values)
+
+if __name__ == "__main__":
+  import dumbo
+  dumbo.run(mapper,reducer)
